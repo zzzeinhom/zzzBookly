221a9a5f5fb1de87f2b09b66609e89d5d2869570
zzzeinhom <mohamedzenhom200@gmail.com>
2025-02-04 22:29:59 +0200

    Add SplashView and SplashViewBody components to the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:zzzbookly/features/splash/presentation/views/splash_view.dart';
 
 void main() {
   runApp(const BooklyApp());
@@ -10,6 +11,8 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp();
+    return const GetMaterialApp(
+      home: SplashView(),
+    );
   }
 }
