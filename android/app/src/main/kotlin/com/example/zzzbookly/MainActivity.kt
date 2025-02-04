2fc4e030383adcd1db0f6662b4c4be0b5e997b83
zzzeinhom <mohamedzenhom200@gmail.com>
2025-02-04 21:53:22 +0200

    Add initial Flutter project structure and configuration files

---
+++ android/app/src/main/kotlin/com/example/zzzbookly/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.zzzbookly
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp();
+  }
+}
