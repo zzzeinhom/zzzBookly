87188b0717bb25ec2e63aa9ce46f3d82e7dce8f6
zzzeinhom <mohamedzenhom200@gmail.com>
2025-02-05 18:08:46 +0200

    finsh splash animation and navigation

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:get/get_core/get_core.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 import 'package:zzzbookly/consts/data.dart';
+import 'package:zzzbookly/features/home/presentaion/views/home_view.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -19,29 +22,7 @@ class _SplashViewBodyState extends State<SplashViewBody>
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 1000),
-    );
-
-    _opacityAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
-      CurvedAnimation(
-        parent: _controller,
-        curve: Curves.easeInOut,
-      ),
-    );
-
-    _slideAnimation = Tween<Offset>(
-      begin: const Offset(0, 0.5),
-      end: Offset.zero,
-    ).animate(
-      CurvedAnimation(parent: _controller, curve: Curves.decelerate),
-    );
-
-    // Start animation after build completes
-    SchedulerBinding.instance.addPostFrameCallback((_) {
-      _controller.forward();
-    });
+    initOpacitySlidingAnimation();
   }
 
   @override
@@ -92,7 +73,12 @@ class _SplashViewBodyState extends State<SplashViewBody>
                 ),
                 SizedBox(height: MediaQuery.of(context).size.height * 0.1),
                 FloatingActionButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    Get.to(
+                      () => const HomeView(),
+                      transition: Transition.rightToLeft,
+                    );
+                  },
                   shape: const StadiumBorder(),
                   child: const Icon(Icons.arrow_forward_rounded),
                 ),
@@ -102,5 +88,31 @@ class _SplashViewBodyState extends State<SplashViewBody>
         ),
       ],
     );
+  }
+
+  void initOpacitySlidingAnimation() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1000),
+    );
+
+    _opacityAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeInOut,
+      ),
+    );
+
+    _slideAnimation = Tween<Offset>(
+      begin: const Offset(0, 0.5),
+      end: Offset.zero,
+    ).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.decelerate),
+    );
+
+    // Start animation after build completes
+    SchedulerBinding.instance.addPostFrameCallback((_) {
+      _controller.forward();
+    });
   }
 }
