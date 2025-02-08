7aa788437b1747ef8c5839aefe8e317c8bae020d
zzzeinhom <mohamedzenhom200@gmail.com>
2025-02-08 21:19:14 +0200

    create home repo with its functions;

---
+++ lib/features/home/data/repos/home_repo.dart
@@ -0,0 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:zzzbookly/core/errors/failures.dart';
+import 'package:zzzbookly/features/home/data/models/book_model/book_model.dart';
+
+abstract class HomeRepo {
+  Future<Either<Failure, List<BookModel>>> fetchTrandingBooks();
+  Future<Either<Failure, List<BookModel>>> fetchMyFavoriteBooks();
+}
+
